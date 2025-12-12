e6272fd3355baddeb8279fb6ebe3c0896c6248d9
Fifilho <tyrone.filho.dev@gmail.com>
2025-12-11 22:30:35 -0300

    Mudanca de valores

--- pizza.py
+++ pizza.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as graf
 def rodar():
-    emocoes = ["UAUAU","Tedio","Felicidade","Preguiça","Sono"]
-    porcentagens = [50,50,50,50,50]
-    kabumm = [0,0,0.05,0,0]
+    emocoes = ["Tesao","Tedio","Felicidade","Preguiça","Sono"]
+    porcentagens = [80,20,20,20,20]
+    kabumm = [0.1,0,0.,0,0]
 
     graf.title("Minhas Emocoes")
     graf.pie(porcentagens, labels=emocoes,explode=kabumm,autopct="%.1f%%",pctdistance=0.7,startangle=90) #frame radius counterclock
